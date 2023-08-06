08701e521700e20372a008b50ef0e3f77ccb27f9
7208sagar <89302157+7208sagar@users.noreply.github.com>
2023-08-06 16:32:46 +0530

    Laptop keyboard HTML CSS

---
+++ laptopkeyboard.js
@@ -0,0 +1,73 @@
+let currentInput = '';
+let current_operator = '';
+let pre_inp = '';
+
+function laptopKeyPress(num) {
+    currentInput += num;
+    updateDisplay();
+    console.log("key Press==",num)
+}
+
+function allOperator(operator) {
+    if (currentInput === '') return;
+    if (pre_inp !== '') {
+        calculation();
+        // console.log(allOperator)
+    }
+    current_operator = operator;
+    pre_inp = currentInput;
+    currentInput = '';
+}
+function calculation() {
+    if (pre_inp === '' || currentInput === '') return;
+    const first_num = parseFloat(pre_inp);
+    const second_num = parseFloat(currentInput);
+    let result = 0;
+    switch (current_operator) {
+        case '+':
+            result = first_num + second_num;
+            break;
+            case '-':
+            result = first_num - second_num;
+            break;
+            case '*':
+            result = first_num * second_num;
+            break;
+            case '/':
+            result = first_num / second_num;
+            break;
+            case '':
+            
+
+    }
+
+    currentInput=result.toString();
+    pre_inp='';
+    current_operator='';
+    updateDisplay();
+}
+function clearResult(){
+currentInput ='';
+current_operator='';
+pre_inp='';
+updateDisplay();
+}
+
+
+function updateDisplay(){
+  document.getElementById('result').value=currentInput;
+}
+
+function backspace(){
+    var deleteInputkey  = document.getElementById('result').value;
+    document.getElementById('result').value=deleteInputkey.substring(0,deleteInputkey.length -1);
+}
+
+
+var input = document.getElementById("result");
+function addSpace(input){
+    if(input.value.length > 1){
+        input.value += " ";
+    }
+
+}
